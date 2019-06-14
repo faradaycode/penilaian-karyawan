58c951c1d104432b66febd7bc352b12aca7e56cd
ferdi <ferdireza96@gmail.com>
2019-06-14 11:05:00 +0700

    make partials for easy customize

---
+++ partials/header.php
@@ -0,0 +1,22 @@
+<head>
+    <meta name="description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <!-- FB meta-->
+    <meta content='https://www.facebook.com/rezaferdi.simple' property='article:author'/>
+    <!-- Generator Meta-->
+    <meta name="title" content="penilaian-karyawan">
+    <meta name="description" content="web app untuk penilaian karyawan memakai algoritma c45 untuk pengambilan keputusan">
+    <meta name="keywords" content="penilaian-karyawan, skripsi, c45, ubhara-jaya">
+    <meta name="robots" content="noindex, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="author" content="verdition96">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
---
+++ partials/sidemenu.php
@@ -0,0 +1,40 @@
+<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image">
+        <div>
+            <p class="app-sidebar__user-name">John Doe</p>
+            <p class="app-sidebar__user-designation">Frontend Developer</p>
+        </div>
+    </div>
+    <ul class="app-menu">
+        <li><a class="app-menu__item active" href="index.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+        <!--dropdown -->
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-users"></i>
+                <span class="app-menu__label">Master Karyawan</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li>
+                    <a class="treeview-item" href="index.php?page=datakaryawan">
+                        <i class="icon fa fa-star-o"></i>
+                        <span>Data Karyawan</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="index.php?page=datapenilaian">
+                        <i class="icon fa fa-star-o"></i>
+                        <span>Data Penilaian</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="app-menu__item" href="index.php?page=penilaian">
+                <i class="app-menu__icon fa fa-check-circle"></i>
+                <span class="app-menu__label">Penilaian</span>
+            </a>
+        </li>
+    </ul>
+</aside>
