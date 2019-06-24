6b729ea294b6fb5a736acac18d9ff100e909ba06
ferdi <ferdireza96@gmail.com>
2019-06-24 09:42:49 +0700

    add modal for import

--- pages/datakaryawan.php
+++ pages/datakaryawan.php
@@ -1,7 +1,7 @@
 <div class="upper-button">
     <div class="flexbox">
         <div>
-            <button class="btn btn-success">
+            <button class="btn btn-success" data-toggle="modal" data-target="#im-karyawanModal">
                 <i class="fa fa-file"></i>
                 <span>import file csv/xls</span>
             </button>
@@ -53,5 +53,33 @@
                 </tr>
             </tbody>
         </table>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="im-karyawanModal" tabindex="-1" role="dialog" aria-labelledby="importkywLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="importkywLabel">Import Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <form>
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="customFile" multiple lang="ar" dir="rtl">
+                            <label class="custom-file-label text-left" for="customFile">choose files to upload</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
 </div>
